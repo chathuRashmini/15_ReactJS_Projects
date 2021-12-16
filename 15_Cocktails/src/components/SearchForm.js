36345bfeadee2d5187fe7269cd117f231e1d1da4
chathu_08 <chathu.rash97@gmail.com>
2021-12-16 07:42:28 +0530

    Completed the cocktails list display funciton

--- 15_Cocktails/src/components/SearchForm.js
+++ 15_Cocktails/src/components/SearchForm.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+
+  const { setsearchTerm } = useGlobalContext()
+
   return (
     <div>
       <h2>search form component</h2>
--- 15_Cocktails/src/context.js
+++ 15_Cocktails/src/context.js
@@ -6,7 +6,59 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+
+  const [loading, setloading] = useState(true)
+  const [searchTerm, setsearchTerm] = useState('a')
+  const [cocktails, setcocktails] = useState([])
+
+  const fetchDrinks = async () => {
+    setloading(true)
+
+    try {
+      const response = await fetch(`${url}${searchTerm}`)
+      const data = await response.json()
+      const { drinks } = data
+
+      if(drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass
+          }
+        })
+
+        setcocktails(newCocktails)
+      }
+      else {
+        setcocktails([])
+      }
+      setloading(false)
+    } catch (error) {
+      setloading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm])
+
+  return (
+    <AppContext.Provider 
+      value={{
+        loading,
+        setsearchTerm,
+        cocktails
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  )
 }
 
 export const useGlobalContext = () => {
